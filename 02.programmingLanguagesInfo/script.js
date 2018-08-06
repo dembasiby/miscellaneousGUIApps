396760525b7b38746f5013d3237216c6686fa8be
Demba Siby <dembasiby@gmail.com>
2018-08-06 12:37:50 +0000

    Set a new variable truncatedTextLength with a value of 120

--- 02.programmingLanguagesInfo/script.js
+++ 02.programmingLanguagesInfo/script.js
@@ -1,19 +1,20 @@
 window.addEventListener('DOMContentLoaded', () => {
   const p = document.querySelectorAll('p');
+  const truncatedTextLength = 120;
 
   for (let i = 0; i < p.length; i += 1) {
     let initialText = p[i].textContent;
-    let truncatedText = p[i].textContent.slice(0, 120) + ' ...';
+    let truncatedText = p[i].textContent.slice(0, truncatedTextLength) + ' ...';
     let buttons = document.querySelectorAll('button');
     let button = buttons[i];
     button.style.display = 'block';
 
-    if (initialText.length > 120) {
+    if (initialText.length > truncatedTextLength) {
       p[i].textContent = truncatedText;
     } else {
       button.style.display = 'none';
     }
-    
+
     button.addEventListener('click', () => {
       if (button.textContent === 'Show more') {
         button.textContent = 'Show less';
