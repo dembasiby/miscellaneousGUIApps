0d3a73fedd0613e3fa41f56aff7463d8e2a3eb3c
Demba Siby <dembasiby@gmail.com>
2018-08-06 21:51:41 +0000

    Added the delete todo functionality

---
+++ 03.customDeleteConfirmationPrompt/js/script.js
@@ -0,0 +1,22 @@
+var todo_items = [{id: 1, title: 'Homework'},
+              {id: 2, title: 'Shopping'},
+              {id: 3, title: 'Calling Mom'},
+              {id: 4, title: 'Coffee with John'}];
+
+var todos = document.getElementById('todos').innerHTML;
+var template = Handlebars.compile(todos);
+var ul = document.getElementById('displayTodos');
+
+todo_items.forEach(todo => {
+    var html = template(todo);
+    ul.innerHTML += html;
+});
+
+var spans = document.querySelectorAll('li span');
+Array.prototype.slice.call(spans).forEach(function(span) {
+    span.addEventListener('click', function(e) {
+        e.target.parentNode.remove();
+    })
+})
+
+
