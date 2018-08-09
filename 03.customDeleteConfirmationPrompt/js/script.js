7aa9c835319db51a7d8adcde8b851158749ef162
Demba Siby <dembasiby@gmail.com>
2018-08-09 22:24:21 +0000

    Replaced es6 code by es5

--- 03.customDeleteConfirmationPrompt/js/script.js
+++ 03.customDeleteConfirmationPrompt/js/script.js
@@ -1,4 +1,4 @@
-var todo_items = [{id: 1, title: 'Homework'},
+var todoItems = [{id: 1, title: 'Homework'},
               {id: 2, title: 'Shopping'},
               {id: 3, title: 'Calling Mom'},
               {id: 4, title: 'Coffee with John'}];
@@ -7,19 +7,19 @@ var todos = document.getElementById('todos').innerHTML;
 var template = Handlebars.compile(todos);
 var ul = document.getElementById('displayTodos');
 
-todo_items.forEach(todo => {
-  let html = template(todo);
+todoItems.forEach(function(todo) {
+  var html = template(todo);
   ul.innerHTML += html;
 });
 
 var spans = document.querySelectorAll('li span');
 
-Array.prototype.slice.call(spans).forEach((span) => {
-  span.addEventListener('click', (e) => {
+Array.prototype.slice.call(spans).forEach(function (span) {
+  span.addEventListener('click', function (e) {
     if (confirm('Are you sure you want to delete this todo?')) {
       e.target.parentNode.remove();
     }
-  })
-})
+  });
+});
 
 
