ab521200b6ecd13b70b87c1728f487ee67cea70c
Demba Siby <dembasiby@gmail.com>
2018-08-11 19:23:07 +0000

    Added the LS solution code as comment, for reference

--- 03.customDeleteConfirmationPrompt/js/script.js
+++ 03.customDeleteConfirmationPrompt/js/script.js
@@ -2,21 +2,23 @@ var todoItems = [{id: 1, title: 'Homework'},
               {id: 2, title: 'Shopping'},
               {id: 3, title: 'Calling Mom'},
               {id: 4, title: 'Coffee with John'}];
-
 var todos = document.getElementById('todos').innerHTML;
 var template = Handlebars.compile(todos);
-var ul = document.getElementById('displayTodos');
-var confirm = document.querySelector('#confirm');
 var yes = document.querySelector('.confirmYes');
 var no = document.querySelector('.confirmNo');
 
 function confirmStyle(value) {
-  confirm.style.display = value;
+  document.querySelector('#confirm').style.display = value;
+}
+
+function deleteTodo(event) {
+  event.target.parentNode.remove();
+  confirmStyle('none');
 }
 
 todoItems.forEach(function(todo) {
   var html = template(todo);
-  ul.innerHTML += html;
+  document.getElementById('displayTodos').innerHTML += html;
 });
 
 var spans = document.querySelectorAll('li span');
@@ -26,8 +28,7 @@ Array.prototype.slice.call(spans).forEach(function (span) {
     confirmStyle('block');
 
     yes.addEventListener('click', function() {
-      e.target.parentNode.remove();
-      confirmStyle('none');
+      deleteTodo(e);
     });
 
     no.addEventListener('click', function() {
@@ -35,5 +36,68 @@ Array.prototype.slice.call(spans).forEach(function (span) {
     });
   });
 });
+
+// LaunchSchool Solution
+
+// var App = {
+//   todos: todoItems,
+//   todosTemplate: Handlebars.compile($('#todos_template').html()),
+//   confirmTemplate: Handlebars.compile($('#confirm_template').html()),
+//   $todos: $('ul#todos'),
+//   $confirm: $('.confirm_prompt'),
+
+//   renderTodos: function() {
+//     this.$todos.html(this.todosTemplate({ todos: this.todos }));
+//   },
+
+//   handleDeleteClick: function(e) {
+//     e.preventDefault();
+//     var todoId   = Number($(e.target).closest('li').attr('data-id'));
+//     this.showPrompt(todoId);
+//   },
+
+//   handleConfirmYes: function(e) {
+//     var todoId = Number($(e.target).closest('.confirm_wrapper').attr('data-id'));
+//     e.preventDefault();
+//     this.removeTodo(todoId);
+//   },
+
+//   removeTodo: function(id) {
+//     this.todos = this.todos.filter(function(todo) {
+//       return todo.id !== id;
+//     });
+
+//     this.hidePrompt();
+//     this.renderTodos();
+//   },
+
+//   showPrompt: function(todoId) {
+//     this.$confirm.html(this.confirmTemplate({id: todoId}));
+//     this.$confirm.add('.overlay').show();
+//     this.bindPromptEvents();
+//   },
+
+//   bindPromptEvents: function() {
+//     this.$confirm.find('.confirm_no').one('click', this.hidePrompt.bind(this));
+//     this.$confirm.find('.confirm_yes').one(
+//       'click',
+//       this.handleConfirmYes.bind(this)
+//     );
+//   },
+
+//   hidePrompt: function() {
+//     this.$confirm.add('.overlay').hide();
+//     this.$confirm.html('');
+//   },
+
+//   init: function() {
+//     this.renderTodos();
+//     this.$todos.on('click', 'li .remove', this.handleDeleteClick.bind(this));
+//     $('.overlay').on('click', this.hidePrompt.bind(this));
+//   }
+// };
+
+// App.init();
+
 
 
