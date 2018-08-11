ee45b4a3fc6b6dbff704195b887c8e2454bcd04e
Demba Siby <dembasiby@gmail.com>
2018-08-11 20:47:52 +0000

    Attached the contextmenu event listener to the todo items

--- 04.contextMenuForTodoList/js/script.js
+++ 04.contextMenuForTodoList/js/script.js
@@ -19,10 +19,12 @@ function deleteTodo(event) {
 var html = template({ todos: todoItems });
 document.getElementById('displayTodos').innerHTML += html;
 
-var spans = document.querySelectorAll('li span');
+var lis = document.querySelectorAll('#displayTodos li');
 
-Array.prototype.slice.call(spans).forEach(function (span) {
-  span.addEventListener('click', function (e) {
+Array.prototype.slice.call(lis).forEach(function (li) {
+li.addEventListener('contextmenu', function (e) {
+    e.preventDefault();
+    console.log('Right click event occured!');
     confirmStyle('block');
 
     yes.addEventListener('click', function() {
@@ -34,68 +36,4 @@ Array.prototype.slice.call(spans).forEach(function (span) {
     });
   });
 });
-
-// LaunchSchool Solution
-
-// var App = {
-//   todos: todoItems,
-//   todosTemplate: Handlebars.compile($('#todos_template').html()),
-//   confirmTemplate: Handlebars.compile($('#confirm_template').html()),
-//   $todos: $('ul#todos'),
-//   $confirm: $('.confirm_prompt'),
-
-//   renderTodos: function() {
-//     this.$todos.html(this.todosTemplate({ todos: this.todos }));
-//   },
-
-//   handleDeleteClick: function(e) {
-//     e.preventDefault();
-//     var todoId   = Number($(e.target).closest('li').attr('data-id'));
-//     this.showPrompt(todoId);
-//   },
-
-//   handleConfirmYes: function(e) {
-//     var todoId = Number($(e.target).closest('.confirm_wrapper').attr('data-id'));
-//     e.preventDefault();
-//     this.removeTodo(todoId);
-//   },
-
-//   removeTodo: function(id) {
-//     this.todos = this.todos.filter(function(todo) {
-//       return todo.id !== id;
-//     });
-
-//     this.hidePrompt();
-//     this.renderTodos();
-//   },
-
-//   showPrompt: function(todoId) {
-//     this.$confirm.html(this.confirmTemplate({id: todoId}));
-//     this.$confirm.add('.overlay').show();
-//     this.bindPromptEvents();
-//   },
-
-//   bindPromptEvents: function() {
-//     this.$confirm.find('.confirm_no').one('click', this.hidePrompt.bind(this));
-//     this.$confirm.find('.confirm_yes').one(
-//       'click',
-//       this.handleConfirmYes.bind(this)
-//     );
-//   },
-
-//   hidePrompt: function() {
-//     this.$confirm.add('.overlay').hide();
-//     this.$confirm.html('');
-//   },
-
-//   init: function() {
-//     this.renderTodos();
-//     this.$todos.on('click', 'li .remove', this.handleDeleteClick.bind(this));
-//     $('.overlay').on('click', this.hidePrompt.bind(this));
-//   }
-// };
-
-// App.init();
-
-
 
