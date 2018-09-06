a688db5f587598a461da416f3597cdd45395dd0a
Demba Siby <dembasiby@gmail.com>
2018-09-06 22:41:48 +0000

    Completed the stopwatch assignment

--- 07.AJavaScriptStopwatch/js/main.js
+++ 07.AJavaScriptStopwatch/js/main.js
@@ -1,32 +1,63 @@
-$(function() {
-  var milliseconds = 0;
-  var seconds = 0;
-  var minutes = 0;
-  var hours = 0;
+var stopWatch = {
+  milliseconds: 0,
+  seconds: 0,
+  minutes: 0,
+  hours: 0,
+  interVal: null,
+  displayTime: function() {
+    $('#hours').html(this.hours < 10 ? '0' + this.hours : this.hours);
+    $('#minutes').html(this.minutes < 10 ? '0' + this.minutes : this.minutes);
+    $('#seconds').html(this.seconds < 10 ? '0' + this.seconds : this.seconds);
+    $('#milliseconds').html(this.milliseconds < 10 ? '0' + 
+      this.milliseconds : this.milliseconds);
+  },
+  startClock: function(e) {
+    e.preventDefault();
+    this.interVal = setInterval(function() {
+      this.milliseconds += 1
+      if (this.milliseconds == 99) {
+        this.seconds += 1;
+        this.milliseconds = 0;
+      }
+      if (this.seconds == 59) {
+        this.minutes += 1;
+        this.seconds = 0;
+      }
+      if (this.minutes == 59) {
+        this.hours += 1;
+        this.minutes = 0;
+      }
+      
+      this.displayTime();
+    }.bind(this), 10);
+    $('.start').html('Stop').removeClass('start').addClass('stop');
+  },
+  stopClock: function(e) {
+    e.preventDefault();
+    clearInterval(this.interVal);
+    $('.stop').html('Start').removeClass('stop').addClass('start');
+  },
+  resetClock: function(e) {
+    e.preventDefault();
+    $('.stop, .start').html('Start')
+      .removeClass('stop').addClass('start');
+    
+    clearInterval(this.interVal);
+    this.milliseconds = 0;
+    this.seconds = 0;
+    this.minutes = 0;
+    this.hours = 0;
+    this.displayTime();
+  },
+  
+  bind: function() {
+    $('#buttons').on('click', '.start', $.proxy(this.startClock, this));
+    $('#buttons').on('click', '.stop', $.proxy(this.stopClock, this));
+    $('#buttons').on('click', '#reset', $.proxy(this.resetClock, this));
+  },
+  init: function() {
+    this.bind();
+  }
+}
 
-  window.setInterval(function() {
-    milliseconds += 1
-
-    if (milliseconds == 100) {
-      seconds += 1;
-      milliseconds = 0;
-    }
-
-    if (seconds == 60) {
-      minutes += 1;
-      seconds = 0;
-    }
-
-    if (minutes == 60) {
-      hours += 1;
-      minutes = 0;
-    }
-
-      $('#hours').html(hours < 10 ? '0' + hours : hours);
-      $('#minutes').html(minutes < 10 ? '0' + minutes : minutes);
-      $('#seconds').html(seconds < 10 ? '0' + seconds : seconds);
-      $('#milliseconds').html(milliseconds);
-
-  }, 10)
-
-})
+stopWatch.init();
